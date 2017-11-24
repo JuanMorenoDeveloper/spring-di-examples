58525b551d5d7dd48c0bf9ded61d268443b1c10a
Juan M.vi <earth001@gmail.com>
2017-11-24 06:22:43 -0300

    Add @SupressWarning and circleci config.

--- src/main/java/ve/com/proitcsolution/service/DatabaseService.java
+++ src/main/java/ve/com/proitcsolution/service/DatabaseService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 public class DatabaseService {
 
   private DataSource dataSource;
+  @SuppressWarnings("unused")
   private AuditService auditService;
 
   @Autowired
